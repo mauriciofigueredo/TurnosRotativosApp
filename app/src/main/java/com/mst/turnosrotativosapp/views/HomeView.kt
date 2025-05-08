ae4bddc1b1e237f834a48eb71f260912caa21b03
Mauricio Figueredo <mauriciofigueredo46@gmail.com>
2025-05-08 17:47:04 -0300

    Prueba LazyColumn en HomeView

--- app/src/main/java/com/mst/turnosrotativosapp/views/HomeView.kt
+++ app/src/main/java/com/mst/turnosrotativosapp/views/HomeView.kt
@@ -3,11 +3,15 @@ package com.mst.turnosrotativosapp.views
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -41,7 +45,12 @@ fun HomeContent(paddingValues: PaddingValues, navController: NavController, pers
         modifier = Modifier
             .padding(paddingValues)
     ) {
-        Text("Home View")
+        val personalList by personalVM.personalList.collectAsState()
+        LazyColumn {
+            items(personalList) {item ->
+                Text(text = item.toString())
+            }
+        }
 
     }
 }
